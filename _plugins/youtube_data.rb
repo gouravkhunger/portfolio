b27bad4c998f6ee147e472a424549acd35d48a6e
Gourav Khunger <gouravkhunger18@gmail.com>
2023-03-26 19:26:13 +0530

    chore: more updates to video box rendering logic

--- _plugins/youtube_data.rb
+++ _plugins/youtube_data.rb
@@ -8,6 +8,7 @@ module Jekyll
   module YoutubeData
     def params(hash = {})
       Dotenv.load
+
       {
         :key => ENV['YOUTUBE_API_KEY'],
         :channelId => 'UCkv-J_D8jK2N02nBcyM92mQ'
@@ -19,8 +20,8 @@ module Jekyll
 
       uri.query = URI.encode_www_form(params({
         order: 'date',
-        maxResults: 50,
-        part: 'snippet',
+        maxResults: 2,
+        part: 'id,snippet',
         pageToken: page_token
       }))
 
@@ -28,6 +29,9 @@ module Jekyll
     end
 
     def videos(input)
+
+      return [{"title"=>"Google Associate Android Developer Certification || My journey of becoming AAD || Exam Review", "date"=>"2022-05-12T160500Z", "description"=>"I became one of the 400 Google Associate Android Developers worldwide and here's everything about it! #google #android ...", "thumbnail"=>{"url"=>"https://i.ytimg.com/vi/tuYFw6f340Y/mqdefault.jpg", "width"=>480, "height"=>360}}, {"title"=>"Compiling and Installing ARM64 native OBS Studio on M1 MAC (Apple Silicon) | M1 Max/Pro/Air", "date"=>"2022-05-09T092111Z", "description"=>"Pre-requisites - Use the arm64 terminal. - Have brew installed in /opt/homebrew prefix. Run these commands in sequence to ...", "thumbnail"=>{"url"=>"https://i.ytimg.com/vi/MD_K4ihR3XI/mqdefault.jpg", "width"=>480, "height"=>360}}]
+
       res = Net::HTTP.get_response(videos_endpoint(''))
       return input if !res.is_a?(Net::HTTPSuccess)
 
@@ -41,7 +45,7 @@ module Jekyll
             'title': item['snippet']['title'],
             'date': item['snippet']['publishedAt'],
             'description': item['snippet']['description'],
-            'thumbnail': item['snippet']['thumbnails']['high']
+            'thumbnail': item['snippet']['thumbnails']['medium']
           })
 
         end
@@ -63,12 +67,14 @@ module Jekyll
               'title': item['snippet']['title'],
               'date': item['snippet']['publishedAt'],
               'description': item['snippet']['description'],
-              'thumbnail': item['snippet']['thumbnails']['high']
+              'thumbnail': item['snippet']['thumbnails']['medium']
             })
 
           end
         end
       end
+
+      puts videos
 
       videos
     end
